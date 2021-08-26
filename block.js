e2fa1c9e37f35036cabf84e8014fc4c6e95f5909
Ronak Desai <ds_rnk@yahoo.co.in>
2021-08-26 18:04:47 -0400

    Update block.js

--- block.js
+++ block.js
@@ -1,15 +1,12 @@
-/// device-width.js
+/// meta.js
 (function () {
-	const start = () => {
-		let view = document.querySelector('meta[name=viewport]')
-		if (!view) {
-			view = document.createElement('meta')
-			view.setAttribute('name', 'viewport')
-			document.getElementsByTagName('head')[0].appendChild(view)
-		}
-		view.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0')
+	const meta = (val = {
+		name: 'viewport',
+		content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+	}) => {
+		Object.assign(document.querySelector('meta[name=viewport]') ||
+			document.getElementsByTagName('head')[0].appendChild(
+				Object.assign(document.createElement('meta'), { name: val.name })), val)
+		if (document.querySelector('meta[name=viewport]').content !== val.content) meta()
 	}
-	if (document.readyState !== 'complete') self.addEventListener('load', start, { once: true })
-	else if (document.readyState !== 'loading') start()
-	else self.addEventListener('DOMContentLoaded', start, { once: true })
 })();
