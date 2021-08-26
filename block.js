9b1cf23f389ac94893834fd8dc91332a657a497f
Ronak Desai <ds_rnk@yahoo.co.in>
2021-08-26 15:14:39 -0400

    Update block.js

--- block.js
+++ block.js
@@ -5,6 +5,7 @@
 		if (!view) {
 			view = document.createElement('meta')
 			view.setAttribute('name', 'viewport')
+			ocument.getElementsByTagName('head')[0].appendChild(view);
 		}
 		view.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0')
 	}
