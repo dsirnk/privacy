ceb159855e30851deeec64029e32b79b22385307
Ronak Desai <ds_rnk@yahoo.co.in>
2021-09-14 10:17:13 -0400

    Update block.js

--- block.js
+++ block.js
@@ -6,7 +6,7 @@
 	},
 	meta = () => Object.assign(document.querySelector('meta[name=viewport]') ||
 		document.getElementsByTagName('head')[0].appendChild(
-			Object.assign(document.createElement('meta'), { name: val.name })), val))
+			Object.assign(document.createElement('meta'), { name: val.name })), val)
 	self.addEventListener('load', meta, { once: true })
 	self.addEventListener('DOMContentLoaded', meta, { once: true })
 	meta()
