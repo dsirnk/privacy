13111080d6675534e2b92741a230788430115d15
Ronak Desai <ds_rnk@yahoo.co.in>
2021-11-09 02:22:58 -0500

    Update block.js

--- block.js
+++ block.js
@@ -1,12 +1,17 @@
 /// meta.js
 (function () {
-	const val = {
-		name: 'viewport',
-		content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-	},
-	meta = () => console.log('meta', Object.assign(document.querySelector('meta[name=viewport]') ||
-		document.getElementsByTagName('head')[0].appendChild(document.createElement('meta')), val))
+	const meta = () => console.log('meta', Object.assign(
+		document.querySelector('meta[name=viewport]') ||
+		document.getElementsByTagName('head')[0].appendChild(document.createElement('meta')),
+		{
+			name: 'viewport',
+			content: `width=${
+				Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+			}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0`
+		}
+	))
 	self.addEventListener('load', meta)
+	self.addEventListener('resize', meta)
 	self.addEventListener('DOMContentLoaded', meta)
 	meta()
 })();
