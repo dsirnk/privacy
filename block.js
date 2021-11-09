1239f1e4c5cb9bae6536355cceda876b88cbf98e
Ronak Desai <ds_rnk@yahoo.co.in>
2021-11-09 01:34:01 -0500

    Update block.js

--- block.js
+++ block.js
@@ -4,10 +4,9 @@
 		name: 'viewport',
 		content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
 	},
-	meta = () => Object.assign(document.querySelector('meta[name=viewport]') ||
-		document.getElementsByTagName('head')[0].appendChild(
-			Object.assign(document.createElement('meta'), { name: val.name })), val)
-	self.addEventListener('load', meta, { once: true })
-	self.addEventListener('DOMContentLoaded', meta, { once: true })
+	meta = () => console.log(Object.assign(document.querySelector('meta[name=viewport]') ||
+		document.getElementsByTagName('head')[0].appendChild(document.createElement('meta')), val))
+	self.addEventListener('load', meta)
+	self.addEventListener('DOMContentLoaded', meta)
 	meta()
 })();
