944554edb6093d65ff2eb8cd102e1d5a980b36ab
Ronak Desai <ds_rnk@yahoo.co.in>
2025-03-25 12:57:59 -0400

    -<meta> +href.watch

--- block.js
+++ block.js
@@ -3,16 +3,6 @@
 	['load', 'resize', 'DOMContentLoaded']
 		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load))
 
-	/** *
-	console.log('meta', Object.assign(
-		document.querySelector('meta[name=viewport]') ||
-		document.getElementsByTagName('head')[0].appendChild(document.createElement('meta')),
-		{ name: 'viewport', content: `width=${
-			640 || Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-		}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0` }
-	))
-	/** */
-
-	document.querySelectorAll('[href*="watch?v="]')
-		.forEach(e => e.href = e.href.replace('watch?v=', 'watch?quality_dash=worst&v='))
+	console.log('watch', document.querySelectorAll('[href*="watch?v="]')
+		.forEach(e => e.href = e.href.replace('watch?v=', 'watch?quality_dash=worst&v=')))
 })();
