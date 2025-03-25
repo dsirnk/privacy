7d2abf322a3f472bc62bccabbf40c6be5c685a1f
Ronak Desai <ds_rnk@yahoo.co.in>
2025-03-25 13:03:31 -0400

    ^order

--- block.js
+++ block.js
@@ -1,8 +1,7 @@
 /// load.js
 (function load() {
+	console.log('watch', document.querySelectorAll('[href*="watch?v="]')
+		.forEach(e => e.href = e.href.replace('watch?v=', 'watch?quality_dash=worst&v=')));
 	['load', 'resize', 'DOMContentLoaded']
 		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load))
-
-	console.log('watch', document.querySelectorAll('[href*="watch?v="]')
-		.forEach(e => e.href = e.href.replace('watch?v=', 'watch?quality_dash=worst&v=')))
 })();
