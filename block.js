9140ee2a44013bc39b153be1a0e3efc1aae22c5f
Ronak Desai <ds_rnk@yahoo.co.in>
2025-03-25 12:51:50 -0400

    invidious quality worst

--- block.js
+++ block.js
@@ -1,17 +1,18 @@
-/// meta.js
-(function () {
-	const meta = () => console.log('meta', Object.assign(
+/// load.js
+(function load() {
+	['load', 'resize', 'DOMContentLoaded']
+		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load))
+
+	/** *
+	console.log('meta', Object.assign(
 		document.querySelector('meta[name=viewport]') ||
 		document.getElementsByTagName('head')[0].appendChild(document.createElement('meta')),
-		{
-			name: 'viewport',
-			content: `width=${
-				640 || Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-			}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0`
-		}
+		{ name: 'viewport', content: `width=${
+			640 || Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+		}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0` }
 	))
-	self.addEventListener('load', meta)
-	self.addEventListener('resize', meta)
-	self.addEventListener('DOMContentLoaded', meta)
-	meta()
+	/** */
+
+	document.querySelectorAll('[href*="watch?v="]')
+		.forEach(e => e.href = e.href.replace('watch?v=', 'watch?quality_dash=worst&v='))
 })();
