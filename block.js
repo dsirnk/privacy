5664c6809f120b9004f28f77dd226cc0a08f79ca
dsirnk <ds_rnk@yahoo.co.in>
2025-04-06 14:18:46 -0400

    +video_data.quality +log

--- block.js
+++ block.js
@@ -1,10 +1,9 @@
 /// load.js
-(function load() {
-	
+(function load() {;
+	console.log('loaded');
+	const pref = window.video_data?.preferences || {};
 	document.querySelectorAll('[href*="watch?v="]')
-		.forEach(e => e.href = e.href.replace('watch?v=', `watch?quality_dash=${
-			((window.video_data||={}).preferences||={}).quality_dash = '144p'
-		}&v=`));
+		.forEach(e => e.href = e.href.replace('/watch?v=', `/watch?quality=${pref.quality = 'medium'}&quality_dash=${pref.quality_dash = 'worst'}&v=`));
 	['load', 'resize', 'DOMContentLoaded']
-		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load))
+		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load));
 })();
