b6f563ba1c7190124c6c040f186e49558b735bab
dsirnk <ds_rnk@yahoo.co.in>
2025-06-08 19:16:06 -0400

    +video_data.quality dash

--- block.js
+++ block.js
@@ -3,7 +3,7 @@
 	console.log('loaded');
 	const pref = window.video_data?.preferences || {};
 	document.querySelectorAll('[href*="watch?v="]')
-		.forEach(e => e.href = e.href.replace('/watch?v=', `/watch?quality=${pref.quality = 'medium'}&quality_dash=${pref.quality_dash = 'worst'}&v=`));
+		.forEach(e => e.href = e.href.replace('/watch?v=', `/watch?quality=${pref.quality = 'dash'}&quality_dash=${pref.quality_dash = 'worst'}&v=`));
 	['load', 'resize', 'DOMContentLoaded']
 		.map(e => self.removeEventListener(e, load) || self.addEventListener(e, load));
 })();
